da1e4afcfa061f3b17785f9d5c2a892a35da14c3
Evan Bush <e.bush05@gmail.com>
2021-03-11 17:41:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Vanilla Dice
+
+A dice roller designed to share a game of dice with friends.
+Step 1: js- Prompt amount of dice. Output that many random numbers (1,6).
+
+
+Step 2: html- Display random numbers in html. Add picture of dice to each number.
+
+
+Step 3: js- Create Roll button.
+        -create <button> to run roll function. onClick="rollFunction()"
+
+Step 4: CSS- Style Dice Boxes. Add Media Queries. 
+
+
+
+Step 5: js js CSS- Keep Dice, Roll remainder. Bank Button.
+
+Step 6: js- Keep them all, roll all again.
+
+Step 7: js html- Tally Score. Add Rules page. 
+
+Step 8: html CSS js Create Score Box. Create Bank.
---
+++ scripts.js
@@ -0,0 +1,52 @@
+var randomArray = [];
+
+function rollDice () {
+    var randomArray = [] 
+for (let i=0; i < 6; i++) {
+
+    randomArray.push(Math.floor(Math.random() *6) + 1)
+
+    document.querySelector('.face' + i).setAttribute("src","img/dice" + randomArray [i] + ".svg");
+}
+};
+
+
+function holdDice (dice, highlight) {
+    var dice = document.getElementById('seis');
+    
+};
+
+
+// attmpt 3
+// var highlight = document.querySelector('.highlight');
+// var dice = document.querySelector('.dice');
+
+// function hold (dice, highlight) {
+//     if (dice) {
+//         dice = dice.setAttribute("class", "highlight");
+//     };
+//     if (highlight) {
+//         highlight = highlight.setAttribute("class", "dice");
+//     };
+// };
+
+
+// attempt 2
+// const a= document.querySelector(".dice");
+// const b= document.querySelector(".hold");
+
+// function hold(a,b) {
+//    for document.querySelector(".dice") {
+//         setAttribute("class", "hold");
+//     }
+// }
+
+
+// attempt 1
+
+// function toggle (a,b) {
+//     const a= document.querySelector(.dice")
+//     const b= document.querySelector(.hold)
+//     if a = b;
+//     if b = a;
+// }
